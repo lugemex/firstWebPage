df053d22cd0eabb84814a536d1d8dba6fdbf29c5
Augusto Aguirre <lugemex@gmail.com>
2019-08-30 17:18:33 -0500

    boton exit

--- jquery/efectButtons.js
+++ jquery/efectButtons.js
@@ -1,14 +1,12 @@
 var $button=$('button'); //llamada por Etiqueta
 var $articles=$('#articles'); //llamada por ID
-$button.addClass('button'); //a todos los botones se les asigna la clase button de CSS
+$button.addClass('robotsButton'); //a todos los botones se les asigna la clase button de CSS
 
 $button.on('click',function(e){
-    $(this).addClass('btnActive').siblings().removeClass('btnActive');
+    $(this).addClass('btnActive').siblings().removeClass('btnActive');//se agrega la clase btnActive al seleccionado y a los hermanos se les quita
     var $selectedBtn='#'+e.target.id;
     console.log($selectedBtn);
-    // $articles.siblings($selectedBtn).hide();
-    $articles.children($selectedBtn).show(1000).siblings().hide(1000);
-    
+    $articles.children($selectedBtn).show(1000).siblings().hide(1000); 
 });
 
 $articles.children().hide();
